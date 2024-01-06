31492a0fad95985d3237dc01d564620bade5d501
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-01-06 17:00:27 +0530

    ..

--- src/components/Payments/Subscribe.jsx
+++ src/components/Payments/Subscribe.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const Subscribe = () => {
@@ -12,9 +12,15 @@ const Subscribe = () => {
 
 <Box p="4">
     <VStack textAlign={"center"} px="8" mt={"4"} spacing ="8">
-        <Text color={'black'} children={`Join Pro Pack and get access to all content.`}/>
-    <Heading size="md" 
+        <Text  children={`Join Pro Pack and get access to all content.`}/>
+    <Heading size="md"  children={"Rs299 Only"}/>
     </VStack>
+    <Button my="8" w="full" colorScheme={"yellow"} >Buy Now</Button>
+</Box>
+<Box bg="blackAlpha.600" p="4" css={{borderRadius:"0 0 8px 8px"}}>
+    <Heading color={"white"} textTransform="uppercase" size="sm"  children="100% refund at cancellation."/>
+
+    <Text fontSize={"xs"} color="white" children={"*Term & Conditions Apply"}/>
 </Box>
          </VStack>
     </Container>
